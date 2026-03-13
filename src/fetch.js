c63f6e50e14ba21fc2ddd7f9b40415823c88f7c1
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-03-13 15:57:59 +0100

    Start hourly section

--- src/fetch.js
+++ src/fetch.js
@@ -10,16 +10,19 @@ export async function fetchCurrentWeahterData(cityName) {
   const currentAPI = `${apiURL}/current.json?key=${apiKey}&lang=${apiLanguage}&q=${cityName}`;
   const response = await fetch(currentAPI);
   const currentCity = await response.json();
+  console.log(currentCity);
   let currentAttributes = {
     name: currentCity.location.name,
     currentTeperature: formatTemperature(currentCity.current.temp_c),
     condition: currentCity.current.condition.text,
+    maxWindSpeed: currentCity.current.wind_kph,
+    icon: currentCity.current.condition.icon,
   };
   return currentAttributes;
 }
 
-export async function fetchMaxMinData(cityName) {
-  const forecastAPI = `${apiURL}/forecast.json?key=${apiKey}&lang=${apiLanguage}&q=${cityName}`;
+export async function fetchForecastWeatherData(cityName) {
+  const forecastAPI = `${apiURL}/forecast.json?key=${apiKey}&lang=${apiLanguage}&q=${cityName}&days=3`;
   const response = await fetch(forecastAPI);
   const maxMinForecast = await response.json();
   let maxMinAttributes = {
@@ -34,17 +37,15 @@ export async function fetchMaxMinData(cityName) {
 }
 
 export function displayCurrentWeahterData(currentAttributes, maxMinAttributes) {
-  const mainWeatherData = document.getElementsByClassName("main-data")[0];
   const mainWeatherContainer = document.createElement("div");
   mainWeatherContainer.className = "main-data";
   const weatherDetails = `<p class="main-data__city">${currentAttributes.name}</p>
         <p class="main-data__temperature">${currentAttributes.currentTeperature + " °"}</p>
         <p class="main-data__condition">${currentAttributes.condition}</p>
         <div class="main-data__max-min">
-          <span id="max">${"H:" + maxMinAttributes.currentDayMaxTemp + " "}</span>${"T:" + maxMinAttributes.currentDayMinTemp}<span id="min"></span>`;
+<span id="max"></span>${"H:" + maxMinAttributes.currentDayMaxTemp + " "}<span id="min">${"T:" + maxMinAttributes.currentDayMinTemp}</span></div>`;
   mainWeatherContainer.innerHTML = weatherDetails;
   const parentContainer = document.getElementsByClassName("main-container")[0];
-  parentContainer.innerHTML = "";
   parentContainer.appendChild(mainWeatherContainer);
 }
 
@@ -53,4 +54,20 @@ export async function addLoadingStatus(cityName) {
     "loading-status__loading-message",
   )[0];
   loadingMessage.innerHTML = `Wetterdaten für ${cityName} werden geladen...`;
+}
+
+export function displayHourlyWeatherData(currentAttributes) {
+  const hoursContainer = document.createElement("div");
+  hoursContainer.className = "hourly-weather";
+  const hourlyWeatherDetails = `<div class="hourly-weather__text"><span>${"Heute " + currentAttributes.condition + ". "}</span><span>${"Wind bis zu " + currentAttributes.maxWindSpeed + " km/h"}</span></div>
+        <div class="hourly-weather__hours">
+          <p "hourly-wather__hour">Jetzt</p>
+          <img src="${currentAttributes.icon}" class="hourly-weather__icon"/>
+          <p class="hourly-wather__temperature">${currentAttributes.currentTeperature + " °"}</p>
+        </div>
+        </div>
+      </div>`;
+  hoursContainer.innerHTML += hourlyWeatherDetails;
+  const parentContainer = document.getElementsByClassName("main-container")[0];
+  parentContainer.appendChild(hoursContainer);
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import "./index.scss";
 import {
   fetchCurrentWeahterData,
-  fetchMaxMinData,
+  fetchForecastWeatherData,
   addLoadingStatus,
   displayCurrentWeahterData,
+  displayHourlyWeatherData,
 } from "./fetch.js";
 const cityName = "Baden-Baden";
 async function init() {
   addLoadingStatus(cityName);
   const currentResult = await fetchCurrentWeahterData(cityName);
-  const maxMinResult = await fetchMaxMinData(cityName);
+  const maxMinResult = await fetchForecastWeatherData(cityName);
+  const parentContainer = document.getElementsByClassName("main-container")[0];
+  parentContainer.innerHTML = "";
   displayCurrentWeahterData(currentResult, maxMinResult);
+  displayHourlyWeatherData(currentResult);
 }
 
 init();
